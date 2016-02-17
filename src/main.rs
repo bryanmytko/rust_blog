46176c4391f819c7fdba826f7e2d26d2a632019f
Bryan Mytko <bryanmytko@gmail.com>
2016-02-17 11:51:32 -0500

    Extract routes

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,11 @@ extern crate iron;
 extern crate router;
 
 use iron::prelude::*;
-use iron::status;
 
-use router::Router;
 mod config;
 
 fn main() {
-    Iron::new(config::router::router).http("localhost:3000").unwrap();
-
-    fn handler(req: &mut Request) -> IronResult<Response> {
-        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap();
-        Ok(Response::with((status::Ok, format!("Hello Universe. You came here via /{}", query))))
-    }
+    Iron::new(config::routes::routes()).http("localhost:3000").unwrap();
 }
 
 
