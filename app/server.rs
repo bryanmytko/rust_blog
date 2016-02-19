d28a75097cf168cd5b49d936b13352e92fb59d03
Bryan Mytko <bryanmytko@gmail.com>
2016-02-18 22:37:20 -0500

    Include models

--- app/server.rs
+++ app/server.rs
@@ -3,10 +3,13 @@ extern crate iron;
 #[macro_use]
 extern crate router;
 
+extern crate rustc_serialize;
+
 use iron::prelude::*;
 
 pub mod config;
 pub mod controllers;
+pub mod models;
 
 fn main() {
     Iron::new(config::routes::routes()).http("localhost:3000").unwrap();
