b77958bf50436dd58971c826c7ab7484e7472c3d
Bryan Mytko <bryanmytko@gmail.com>
2016-02-19 08:28:46 -0500

    Add Views

--- app/server.rs
+++ app/server.rs
@@ -1,16 +1,26 @@
+#![feature(plugin)]
+#![plugin(maud_macros)]
+
 extern crate iron;
+extern crate rustc_serialize;
+extern crate maud;
 
 #[macro_use]
 extern crate router;
 
-extern crate rustc_serialize;
-
 use iron::prelude::*;
+use std::io;
 
 pub mod config;
-pub mod controllers;
 pub mod models;
+pub mod views;
+pub mod controllers;
 
 fn main() {
-    Iron::new(config::routes::routes()).http("localhost:3000").unwrap();
+    //Iron::new(config::routes::routes()).http("localhost:3000").unwrap();
+
+    let name = "Lyra";
+    html_utf8!(io::stdout(), {
+        p { "Hi, " $name "!" }
+    }).unwrap();
 }
