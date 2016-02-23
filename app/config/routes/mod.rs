44769d779eee33782cd965e05ff674e01184694b
Bryan Mytko <bryanmytko@gmail.com>
2016-02-23 15:02:30 -0500

    Add about#index route

--- app/config/routes/mod.rs
+++ app/config/routes/mod.rs
@@ -1,14 +1,10 @@
-// Supported Methods:
-// get, post, put, delete, head, patch, options and any.
-
 use controllers::*;
 
 pub fn routes() -> Router {
-    // get "/" => root,
-
     router!(
         get "/" => posts_controller::index,
         get "/posts" => posts_controller::index,
-        get "/posts/:id" => posts_controller::show
+        get "/posts/:id" => posts_controller::show,
+        get "/about" => about_controller::index,
     )
 }
