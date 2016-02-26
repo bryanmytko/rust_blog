919bb06a31fa0c680a9367681d3e29cc010dc24d
Bryan Mytko <bryanmytko@gmail.com>
2016-02-25 21:04:40 -0500

    Add dummy data to User via User model

--- app/models/user.rs
+++ app/models/user.rs
@@ -25,17 +25,20 @@ impl User {
       }).unwrap()
   }
 
-    pub fn create(user: User) -> i32 {
+    pub fn create(params: &[String; 5]) -> i32 {
         let conn = db::connection();
 
-        /* @TODO remove: Dummy Data */
-        let email = "bryan@test.com";
-        let first_name = "bryan";
-        let last_name = "mytko";
-        let password_digest = "32r90aweufija4fkljasd;f";
-        let admin = false;
+        let user = User {
+            id: 1,
+            email: "bryan@test.com".to_string(),
+            first_name: "bryan".to_string(),
+            last_name: "mytko".to_string(),
+            password_digest: "32r90aweufija4fkljasd,f".to_string(),
+            admin: false,
+        };
 
-        let result = conn.execute("INSERT INTO user (email, first_name, last_name, password_digest, admin) VALUES ($1, $2, $3, $4, $5)", &[&email, &first_name, &last_name, &password_digest, &admin]);
+        let result = conn.execute("INSERT INTO user (email, first_name, last_name, password_digest, admin) VALUES ($1, $2, $3, $4, $5)", &[&params[0],&params[1],&params[2],&params[3],&params[4]]);
+
         result.unwrap()
     }
 }
